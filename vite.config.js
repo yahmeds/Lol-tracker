0b8e0555579e7db383c60558615cb115590f2003
Ahnia Ahmed Yanis <yanis.ahnia.etu@univ-lille.fr>
2026-04-11 13:19:33 +0200

    init: setup vite + react project

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'CoachScan – LoL Tracker',
+        short_name: 'CoachScan',
+        description: 'Surveillance de joueurs League of Legends pour coaches esport',
+        theme_color: '#07070d',
+        background_color: '#07070d',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ],
+})
